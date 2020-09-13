e6f9ecccad8346a8577a27fcdae6bd7eb8c62409
Vasypu <lesch.kostenko@yandex.ru>
2020-09-14 00:32:50 +0300

    Урок 5 (Внедрение зависимостей с отдельным файлом)

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -2,11 +2,25 @@ package org.example;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int value;
 
     //IoC
     public MusicPlayer (Music music) {
         this.music = music;
     }
+
+    public MusicPlayer () {}
+
+    public void setName(String name) { this.name = name; }
+
+    public void setValue(int value) { this.value = value; }
+
+    public String getName() { return name; }
+
+    public int getValue() { return value; }
+
+    public void setMusic(Music music) { this.music = music; }
 
     public void playMusic () {
         System.out.println("Playing " + music.getSong());
