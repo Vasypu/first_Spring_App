2865772f64cd236fdfd001e102f70e1b156717aa
Vasypu <lesch.kostenko@yandex.ru>
2020-09-21 00:31:14 +0300

    Урок 10 (Анотоцаии в Spring @Qualifier)

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,22 +1,27 @@
 package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
 //    @Autowired
-    private Music music;
-    private ClassicalMusic classicalMusic;
-    private JazzMusic jazzMusic;
+//    @Qualifier("classicalMusic")
+    private Music music1;
+    private Music music2;
+//    private ClassicalMusic classicalMusic;
+//    private JazzMusic jazzMusic;
 
     @Autowired
-    public void setMusic(ClassicalMusic classicalMusic, JazzMusic jazzMusic) {
-        this.classicalMusic = classicalMusic;
-        this.jazzMusic = jazzMusic;
+    public void setMusic(@Qualifier("classicalMusic") Music classicalMusic,
+                         @Qualifier("jazzMusic") JazzMusic jazzMusic) {
+        this.music1 = classicalMusic;
+        this.music2 = jazzMusic;
     }
 
     public String playMusic () {
-        return "Playing " + classicalMusic.getSong() + " " + jazzMusic.getSong();
+//        return "Playing " + classicalMusic.getSong() + " " + jazzMusic.getSong();
+        return "Playing " + music1.getSong() + " " + music2.getSong();
     }
 }
