40d925a10e3186be5de110646fd4a5f06d384054
Vasypu <lesch.kostenko@yandex.ru>
2020-09-20 22:21:25 +0300

    Урок 8 (Анотоцаии в Spring)

--- src/main/java/org/example/JazzMusic.java
+++ src/main/java/org/example/JazzMusic.java
@@ -1,20 +1,23 @@
 package org.example;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class JazzMusic implements Music {
 
-    private JazzMusic () {}
+//    private JazzMusic () {}
 
-    public static JazzMusic getJazzMusic () {
-        return new JazzMusic();
-    }
+//    public static JazzMusic getJazzMusic () {
+//        return new JazzMusic();
+//    }
 
-    public void doMyInit () {
-        System.out.println("Doing init method");
-    }
-
-    public void doMyDestroy () {
-        System.out.println("Doing destroy method");
-    }
+//    public void doMyInit () {
+//        System.out.println("Doing init method");
+//    }
+//
+//    public void doMyDestroy () {
+//        System.out.println("Doing destroy method");
+//    }
 
     @Override
     public String getSong() { return "New music"; }
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -7,13 +7,7 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getValue());
-
-        JazzMusic jazzMusic = context.getBean("musicBeanJazz", JazzMusic.class);
+        JazzMusic jazzMusic = context.getBean("jazzMusic", JazzMusic.class);
         System.out.println(jazzMusic.getSong());
 
         context.close();
